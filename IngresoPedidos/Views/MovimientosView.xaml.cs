fb71a64fc753813f03250355eb1c35ab844a09b1
Jose Pompilio <milveintiuno@gmail.com>
2019-01-18 16:03:26 -0300

    just perfect

--- IngresoPedidos/Views/MovimientosView.xaml.cs
+++ IngresoPedidos/Views/MovimientosView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows;
 
 namespace IngresoPedidos.Views
@@ -18,13 +19,19 @@ namespace IngresoPedidos.Views
             List<string> ListaMovimientos = new List<string>();
 
             // Read the file and display it line by line.  
-            System.IO.StreamReader file = new System.IO.StreamReader(@"c:\test.txt");
-            while ((line = file.ReadLine()) != null)
+            //System.IO.StreamReader file = new System.IO.StreamReader(@"c:\test.txt");
+            //while ((line = file.ReadLine()) != null)
+            //{
+            //    ListaMovimientos.Add(line);
+            //}
+            string[] lines = System.IO.File.ReadAllLines(@"c:\test.txt");
+            lines = lines.Skip(1).ToArray();
+            for (int i = 0; i < lines.Length; i++)
             {
-                ListaMovimientos.Add(line);
+                ListaMovimientos.Add(lines[i]);
             }
 
-            file.Close();
+            //file.Close();
             lbMovimientos.ItemsSource = ListaMovimientos;
 
         

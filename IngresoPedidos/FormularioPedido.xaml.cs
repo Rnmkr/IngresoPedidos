2c73f9a12422168192cbffe57a383f46e75c0e33
José Rnmkr <milveintiuno@outlook.com>
2019-02-06 01:37:19 -0300

    asd

--- IngresoPedidos/FormularioPedido.xaml.cs
+++ IngresoPedidos/FormularioPedido.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using IngresoPedidos.DatabaseContext;
@@ -9,6 +10,7 @@ namespace IngresoPedidos
     {
         PedidosView nuevoPedido;
         PedidosView viejoPedido;
+        public List<string> listaestados = new List<string> { "AUTORIZADO", "CANCELADO", "COMPLETO", "DESPACHADO", "FINALIZADO", "INCOMPLETO", "INGRESADO", "PAUSADO", "PRODUCCION" };
 
         public FormularioPedido()
         {
@@ -16,30 +18,47 @@ namespace IngresoPedidos
             this.Title = "NUEVO PEDIDO";
             cbEstado.SelectedIndex = 0;
             cbEstado.IsEnabled = false;
+            cbEstado.Foreground = System.Windows.Media.Brushes.Gray;
             tbPedido.GotFocus += RemovePlaceholder;
             tbPedido.LostFocus += AddPlaceholder;
             tbArticulo.GotFocus += RemovePlaceholder;
             tbArticulo.LostFocus += AddPlaceholder;
             tbCantidad.GotFocus += RemovePlaceholder;
             tbCantidad.LostFocus += AddPlaceholder;
+            //cbEstado.GotFocus += RemovePlaceholder;
+            //cbEstado.LostFocus += AddPlaceholder;
             tbAnterior.GotFocus += RemovePlaceholder;
             tbAnterior.LostFocus += AddPlaceholder;
+            tbObservacion.GotFocus += RemovePlaceholder;
+            tbObservacion.LostFocus += AddPlaceholder;
         }
 
         public FormularioPedido(PedidosView p)
         {
             InitializeComponent();
+            
+            cbEstado.Foreground = System.Windows.Media.Brushes.Black;
+            tbPedido.Foreground = System.Windows.Media.Brushes.Black;
+            cbModelo.Foreground = System.Windows.Media.Brushes.Black;
+            cbProducto.Foreground = System.Windows.Media.Brushes.Black;
+            tbArticulo.Foreground = System.Windows.Media.Brushes.Black;
+            tbCantidad.Foreground = System.Windows.Media.Brushes.Black;
+            tbAnterior.Foreground = System.Windows.Media.Brushes.Black;
+
+            tbObservacion.GotFocus += RemovePlaceholder;
+            tbObservacion.LostFocus += AddPlaceholder;
             this.Title = "EDITAR PEDIDO";
-            PedidosView pedidoSeleccionado = p;
+            PedidosView pedidoSeleccionado = new PedidosView {IDPedido = 0, Articulo = "2448", CantidadEquipos = 200, EstadoPedido = "AUTORIZADO", FechaEstado = DateTime.Now, FechaIngreso = DateTime.Now, NombreModelo = "H8", NombreProducto = "ALL-IN-ONE", NumeroPedido = "1234567A-00", PedidoAnterior = "1111111A-00", PedidoSucesor ="2222222A-00" };
             tbPedido.Text = pedidoSeleccionado.NumeroPedido;
             tbPedido.IsEnabled = false;
+            cbEstado.ItemsSource = listaestados;
             cbModelo.SelectedValue = pedidoSeleccionado.NombreModelo;
             cbModelo.SelectedValue = pedidoSeleccionado.NombreProducto;
             tbArticulo.Text = pedidoSeleccionado.Articulo;
             tbCantidad.Text = pedidoSeleccionado.CantidadEquipos.ToString();
             cbEstado.SelectedValue = pedidoSeleccionado.EstadoPedido;
             tbAnterior.Text = pedidoSeleccionado.PedidoAnterior;
-            tbObservacion.Focus();
+            //tbObservacion.Focus();
 
         }
 
@@ -64,11 +83,15 @@ namespace IngresoPedidos
 
         private void AddPlaceholder(object sender, EventArgs e)
         {
-            TextBox tb = sender as TextBox;
-            tb.Foreground = System.Windows.Media.Brushes.Gray;
+            tbPedido.Text = tbPedido.Text.ToUpper();
+            tbAnterior.Text = tbAnterior.Text.ToUpper();
+            tbObservacion.Text = tbObservacion.Text.ToUpper();
 
+            TextBox tb = sender as TextBox;
             if (string.IsNullOrWhiteSpace(tb.Text))
             {
+                tb.Foreground = System.Windows.Media.Brushes.LightGray;
+
                 switch (tb.Name)
                 {
                     case "tbPedido":
@@ -87,6 +110,10 @@ namespace IngresoPedidos
                         tb.Text = "ANTERIOR";
                         break;
 
+                    case "tbObservacion":
+                        tb.Text = "OBSERVACION";
+                        break;
+
                     default:
                         break;
                 }
@@ -97,7 +124,6 @@ namespace IngresoPedidos
         {
             TextBox tb = sender as TextBox;
             tb.Foreground = System.Windows.Media.Brushes.Black;
-
             switch (tb.Name)
             {
                 case "tbPedido":
@@ -128,8 +154,54 @@ namespace IngresoPedidos
                     }
                     break;
 
+                case "tbObservacion":
+                    if (tb.Text == "OBSERVACION")
+                    {
+                        tb.Text = "";
+                    }
+                    break;
+
                 default:
                     break;
+            }
+        }
+
+        private void cbModelo_DropDownOpened(object sender, EventArgs e)
+        {
+            cbModelo.Foreground = System.Windows.Media.Brushes.Black;
+        }
+
+        private void cbModelo_DropDownClosed(object sender, EventArgs e)
+        {
+            if (cbModelo.SelectedIndex == -1)
+            {
+                cbModelo.Foreground = System.Windows.Media.Brushes.LightGray;
+            }
+        }
+
+        private void cbProducto_DropDownOpened(object sender, EventArgs e)
+        {
+            cbProducto.Foreground = System.Windows.Media.Brushes.Black;
+        }
+
+        private void cbProducto_DropDownClosed(object sender, EventArgs e)
+        {
+            if (cbProducto.SelectedIndex == -1)
+            {
+                cbProducto.Foreground = System.Windows.Media.Brushes.LightGray;
+            }
+        }
+
+        private void cbEstado_DropDownOpened(object sender, EventArgs e)
+        {
+            cbEstado.Foreground = System.Windows.Media.Brushes.Black;
+        }
+
+        private void cbEstado_DropDownClosed(object sender, EventArgs e)
+        {
+            if (cbEstado.SelectedIndex == -1)
+            {
+                cbEstado.Foreground = System.Windows.Media.Brushes.LightGray;
             }
         }
     }
